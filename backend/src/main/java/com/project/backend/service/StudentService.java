7058352a2d32603aa58e92825b3fa64ef7e990d9
Skrubzeey <jednog000@pbs.edu.pl>
2026-03-19 09:23:43 +0100

    Stworzono implementacje do interfejsów w Serwisach Student i Zadanie.

---
+++ backend/src/main/java/com/project/backend/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.project.backend.service;
+
+import com.project.backend.model.Student;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
+import java.util.Optional;
+
+public interface StudentService {
+    Optional<Student> getStudent(Integer studentId);
+
+    Student setStudent(Student student);
+
+    void deleteStudent(Student studentId);
+
+    Page<Student> getStudenci(Pageable pageable);
+}
