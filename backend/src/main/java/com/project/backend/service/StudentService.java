dff0f4f70992ed568cb13fd8c631a48c4e2b9a7a
Skrubzeey <jednog000@pbs.edu.pl>
2026-03-26 08:37:09 +0100

    Może zadziała fix

--- backend/src/main/java/com/project/backend/service/StudentService.java
+++ backend/src/main/java/com/project/backend/service/StudentService.java
@@ -3,6 +3,7 @@ package com.project.backend.service;
 import com.project.backend.model.Student;
 import org.springframework.data.domain.Page;
 
+
 import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
