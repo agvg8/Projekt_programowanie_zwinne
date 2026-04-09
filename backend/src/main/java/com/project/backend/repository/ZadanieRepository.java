c421d17e0b99b7170ab1e842a4b8a5a6978f82e3
Tlenek Azotu <n2o3.naj@gmail.com>
2026-04-09 09:26:30 +0200

    wywalenie findZadania i poprawienie nazwy pola

--- backend/src/main/java/com/project/backend/repository/ZadanieRepository.java
+++ backend/src/main/java/com/project/backend/repository/ZadanieRepository.java
@@ -15,6 +15,4 @@ public interface ZadanieRepository extends JpaRepository<Zadanie, Integer> {
     @Query("SELECT z FROM Zadanie z WHERE z.projekt.projektId = :projektId")
     List<Zadanie> findZadaniaProjektu(@Param("projektId") Integer projektId);
 
-    @Query("SELECT z FROM Zadanie z")
-    Page<Zadanie> findZadania(Pageable pageable);
 }
