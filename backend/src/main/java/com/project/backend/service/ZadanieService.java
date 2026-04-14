331388ab16f768bbb1f09bc7f008d31ea51b9256
Your Name <jednog000@pbs.edu.pl>
2026-04-14 19:44:29 +0200

    Implementacja metody updateStatus w serwisie

--- backend/src/main/java/com/project/backend/service/ZadanieService.java
+++ backend/src/main/java/com/project/backend/service/ZadanieService.java
@@ -1,5 +1,6 @@
 package com.project.backend.service;
 
+import com.project.backend.model.StatusZadania;
 import com.project.backend.model.Zadanie;
 import org.springframework.data.domain.Page;
 
@@ -16,4 +17,6 @@ public interface ZadanieService {
     void deleteZadanie(Integer zadanieId);
 
     Page<Zadanie> getZadania(Pageable pageable);
+
+    Zadanie updateStatus(Integer zadanieId, StatusZadania status);
 }
