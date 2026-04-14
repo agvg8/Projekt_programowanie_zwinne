474c47d0940077c070cd09333d5ff2f1b654909e
Your Name <jednog000@pbs.edu.pl>
2026-04-14 19:47:10 +0200

    Dodanie nadawania statusu w Controllerze

--- backend/src/main/java/com/project/backend/controller/ZadanieController.java
+++ backend/src/main/java/com/project/backend/controller/ZadanieController.java
@@ -1,5 +1,6 @@
 package com.project.backend.controller;
 
+import com.project.backend.model.StatusZadania;
 import com.project.backend.model.Zadanie;
 import com.project.backend.service.ZadanieServiceImpl;
 import com.project.backend.service.ProjektService;
@@ -55,5 +56,18 @@ public class ZadanieController {
             zadanieService.deleteZadanie(zadanieId);
             return new ResponseEntity<Void>(HttpStatus.OK);
         }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{zadanieId}/status")
+    public ResponseEntity<Void> updateStatus(
+            @PathVariable Integer zadanieId,
+            @RequestParam StatusZadania status
+    ) {
+        return zadanieService.getZadanieOptional(zadanieId)
+                .map(z -> {
+                    zadanieService.updateStatus(zadanieId, status);
+                    return new ResponseEntity<Void>(HttpStatus.OK);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
